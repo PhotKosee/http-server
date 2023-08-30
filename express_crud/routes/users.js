59bdca5da1226ebb8bd331421e1c5cd40928229e
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-08-30 21:34:19 +1000

    add 2 new endpoints

--- express_crud/routes/users.js
+++ express_crud/routes/users.js
@@ -24,12 +24,12 @@ let users = [
 ];
 
 // GET request: Retrieve all users
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
   res.send(JSON.stringify({users}, null, 4));  
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
-router.get("/:email",(req,res)=>{
+router.get("/:email", (req, res) => {
   const email = req.params.email;
   let filtered_users = users.filter((user) => user.email === email);
   res.send(filtered_users);  
@@ -37,7 +37,7 @@ router.get("/:email",(req,res)=>{
 
 
 // POST request: Create a new user
-router.post("/",(req,res)=>{
+router.post("/", (req, res) => {
   users.push({
     firstName: req.query.firstName,
     lastName: req.query.lastName,
@@ -74,12 +74,30 @@ router.put("/:email", (req, res) => {
   }  
 });
 
-
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
   let email = req.params.email;
   users = users.filter((user) => user.email != email);
   res.send(`User with the email  ${email} deleted.`);
+});
+
+// Create an endpoint in the same code for getting all users with a particular Last Name
+app.get("/lastNmae/:lastName", (req, res) => {
+  let lastName = req.params.lastName;
+  let filtered_user = users.map((user) => user.lastName === lastName);
+  res.send(JSON.stringify({filtered_user}, null, 4));
+});
+
+// Create an endpoint in the same code for sorting users by date of birth
+app.put("/sort", (req, res) => {
+  let sorted_users = users.sort(function(a, b) {
+    let [d1, m1, y1] = a.DOB.split("=");
+    let b1 = new Date(y1 + "/" + m1 + "/" + d1);
+    let [d2, m2, y2] = b.DOB.split("=");
+    let b2 = new Date(y2 + "/" + m2 + "/" + d2);
+    return b1 - b2;
+  })
+  res,send(JSON.stringify({sorted_users}, null, 4));
 });
 
 module.exports=router;
